9c0590ec442c79ce3f4725571c555b430f6e66d9
Rafael <rafaelandradetanimoto@gmail.com>
2026-03-22 18:37:26 -0300

    Add Board and Piece classes with basic structure

--- src/main/java/br/com/famel/Main.java
+++ src/main/java/br/com/famel/Main.java
@@ -1,13 +1,13 @@
 package br.com.famel;
 
+import br.com.famel.boardgame.Board;
 import br.com.famel.boardgame.Position;
 import static java.lang.IO.println;
 
 public class Main {
     static void main() {
 
-        Position position = new Position(1, 2);
-        println(position);
+        Board board = new Board(8, 8);
 
     }
 }
--- src/main/java/br/com/famel/boardgame/Piece.java
+++ src/main/java/br/com/famel/boardgame/Piece.java
@@ -1,4 +1,15 @@
 package br.com.famel.boardgame;
 
 public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
