668150ed80b39a964140ec4e50004d5669908597
Rafael <rafaelandradetanimoto@gmail.com>
2026-04-08 20:16:11 -0300

    Add ChessPosition and ChessException classes, refactor ChessMatch for piece placement logic

--- src/main/java/br/com/famel/chess/ChessMatch.java
+++ src/main/java/br/com/famel/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package br.com.famel.chess;
 
-import br.com.famel.UI;
 import br.com.famel.boardgame.Board;
-import br.com.famel.boardgame.Position;
 import br.com.famel.chess.pieces.King;
 import br.com.famel.chess.pieces.Rook;
 
@@ -25,9 +23,13 @@ public class ChessMatch {
         return pieces;
     }
 
+    public void placeNewPiece(char column, int row,  ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
