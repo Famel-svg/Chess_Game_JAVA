b2c48995e7af680c37559ff60a8c5e69715eb4c3
Rafael <rafaelandradetanimoto@gmail.com>
2026-04-15 18:43:59 -0300

    Add abstract method for possible moves in Piece, refine move validation logic, and implement basic move logic for King and Rook

--- src/main/java/br/com/famel/chess/ChessPiece.java
+++ src/main/java/br/com/famel/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package br.com.famel.chess;
 import br.com.famel.boardgame.Board;
 import br.com.famel.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
