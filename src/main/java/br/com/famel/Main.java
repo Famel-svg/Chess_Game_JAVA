39f98b336f791e97d35dd8fdfef46668ae690a49
Rafael <rafaelandradetanimoto@gmail.com>
2026-04-15 18:15:09 -0300

    Add exception handling in main game loop, improve user feedback, and add screen clearing utility in UI

--- src/main/java/br/com/famel/Main.java
+++ src/main/java/br/com/famel/Main.java
@@ -2,12 +2,11 @@ package br.com.famel;
 
 import br.com.famel.boardgame.Board;
 import br.com.famel.boardgame.Position;
+import br.com.famel.chess.ChessException;
 import br.com.famel.chess.ChessMatch;
 import br.com.famel.chess.ChessPiece;
 import br.com.famel.chess.ChessPosition;
-
 import java.util.Scanner;
-
 import static java.lang.IO.println;
 
 public class Main {
@@ -17,16 +16,22 @@ public class Main {
         ChessMatch match = new ChessMatch();
 
         while (true){
-            UI.printBoard(match.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(new Scanner(sc.nextLine().toUpperCase()));
+            try {
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(new Scanner(sc.nextLine().toUpperCase()));
 
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(new Scanner(sc.nextLine().toUpperCase()));
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(new Scanner(sc.nextLine().toUpperCase()));
 
-            ChessPiece capturedPiece = match.performChessMove(source, target);
+                ChessPiece capturedPiece = match.performChessMove(source, target);
+            }
+            catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
